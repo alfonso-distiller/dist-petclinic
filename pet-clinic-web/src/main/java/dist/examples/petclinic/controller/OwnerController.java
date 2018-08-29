e003ca314ef9d09b888db1bc197a2a7d8aed3a04
alfonso-distiller <alfonso.delrio@live.com.mx>
2018-08-28 20:30:26 -0500

    Now the index of contorller will list the owners
    Closes #21

--- pet-clinic-web/src/main/java/dist/examples/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/dist/examples/petclinic/controller/OwnerController.java
@@ -1,13 +1,24 @@
 package dist.examples.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import dist.examples.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"","/","/index","/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
